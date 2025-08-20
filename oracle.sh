3806efa894c665ab8f62db64e415b56136c4e2eb
Polaris <fdyrwaas@outlook.com>
2025-08-21 07:48:56 +0800

    Update oracle.sh

--- oracle.sh
+++ oracle.sh
@@ -13,69 +13,69 @@ RESET="\033[0m"
 menu() {
     clear
     echo -e "${GREEN}=== 甲骨文管理菜单 ===${RESET}"
-    echo -e "${GREEN}01) 开启 ROOT 登录${RESET}"
-    echo -e "${GREEN}02) 一键重装系统${RESET}"
-    echo -e "${GREEN}03) 恢复 IPv6${RESET}"
-    echo -e "${GREEN}04) 安装保活 Oracle${RESET}"
-    echo -e "${GREEN}05) 安装 lookbusy 保活${RESET}"
-    echo -e "${GREEN}06) 安装 R 探针${RESET}"
-    echo -e "${GREEN}07) y 探长${RESET}"
-    echo -e "${GREEN}08) OCI 抢机${RESET}"
-    echo -e "${GREEN}09) 计算圆周率${RESET}"
-    echo -e "${GREEN}10) 甲骨文救砖${RESET}"
+    echo -e "${GREEN}01) 甲骨文救砖${RESET}"
+    echo -e "${GREEN}02) 开启 ROOT 登录${RESET}"
+    echo -e "${GREEN}03) 一键重装系统${RESET}"
+    echo -e "${GREEN}04) 恢复 IPv6${RESET}"
+    echo -e "${GREEN}05) 安装保活 Oracle${RESET}"
+    echo -e "${GREEN}06) 安装 lookbusy 保活${RESET}"
+    echo -e "${GREEN}07) 安装 R 探针${RESET}"
+    echo -e "${GREEN}08) y 探长${RESET}"
+    echo -e "${GREEN}09) OCI 抢机${RESET}"
+    echo -e "${GREEN}10) 计算圆周率${RESET}"
     echo -e "${GREEN}0)  退出${RESET}"
     echo
     read -p $'\033[32m请选择操作 (0,01-10): \033[0m' choice
 
     case $choice in
         01)
+            echo -e "${GREEN}正在执行甲骨文救砖...${RESET}"
+            bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/ocijz.sh)
+            pause
+            ;;
+        02)
             echo -e "${GREEN}正在开启 ROOT 登录...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/iu683/unblock/main/xgmim.sh)
             pause
             ;;
-        02)
+        03)
             echo -e "${GREEN}正在一键重装系统...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/ddoci.sh)
             pause
             ;;
-        03)
+        04)
             echo -e "${GREEN}正在恢复 IPv6...${RESET}"
             bash <(curl -L -s jhb.ovh/jb/v6.sh)
             pause
             ;;
-        04)
+        05)
             echo -e "${GREEN}正在安装保活 Oracle...${RESET}"
             bash <(wget -qO- --no-check-certificate https://gitlab.com/spiritysdx/Oracle-server-keep-alive-script/-/raw/main/oalive.sh)
             pause
             ;;
-        05)
+        06)
             echo -e "${GREEN}正在安装 lookbusy 保活...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/lookbusy.sh)
             pause
             ;;
-        06)
+        07)
             echo -e "${GREEN}正在安装 R 探针...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/roci.sh)
             pause
             ;;
-        07)
+        08)
             echo -e "${GREEN}正在运行 y 探长...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/iu683/vps-tools/main/oci-helper_install.sh)
             pause
             ;;
-        08)
+        09)
             echo -e "${GREEN}正在运行 OCI 抢机...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/iu683/vps-tools/main/oci-docker.sh)
             pause
             ;;
-        09)
+        10)
             echo -e "${GREEN}正在计算圆周率...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/jsyzl.sh)
-            pause
-            ;;
-        10)
-            echo -e "${GREEN}正在执行甲骨文救砖...${RESET}"
-            bash <(curl -fsSL https://raw.githubusercontent.com/iu683/unblock/main/ocijz.sh)
             pause
             ;;
         0)
