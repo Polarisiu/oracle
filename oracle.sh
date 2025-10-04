3b9d60fb864566df74ba59f2073f7ef98c072718
Polaris <fdyrwaas@outlook.com>
2025-10-04 23:50:08 +0800

    Update oracle.sh

--- oracle.sh
+++ oracle.sh
@@ -4,9 +4,11 @@
 # 颜色定义
 # =============================
 GREEN="\033[32m"
-RED="\033[31m"
 YELLOW="\033[33m"
+RED="\033[31m"
+BLUE="\033[34m"
 RESET="\033[0m"
+BOLD="\033[1m"
 
 # =============================
 # 脚本路径
@@ -26,21 +28,20 @@ pause() {
 # 菜单函数
 # =============================
 menu() {
-    echo -e "${GREEN}======甲骨文管理菜单======${RESET}"
-    echo -e "${GREEN}[01] 甲骨文救砖${RESET}"
-    echo -e "${GREEN}[02] 开启 ROOT 登录${RESET}"
-    echo -e "${GREEN}[03] 一键重装系统${RESET}"
-    echo -e "${GREEN}[04] 恢复 IPv6${RESET}"
-    echo -e "${GREEN}[05] 安装保活 Oracle${RESET}"
-    echo -e "${GREEN}[06] 安装 lookbusy 保活${RESET}"
-    echo -e "${GREEN}[07] 安装 Y 探长${RESET}"
-    echo -e "${GREEN}[08] 安装 oci-start${RESET}"
-    echo -e "${GREEN}-------------------------${RESET}"
+    echo -e "${BLUE}====== 甲骨文管理菜单 ======${RESET}"
+    echo -e "${YELLOW}[01] 甲骨文救砖${RESET}"
+    echo -e "${YELLOW}[02] 开启 ROOT 登录${RESET}"
+    echo -e "${YELLOW}[03] 一键重装系统${RESET}"
+    echo -e "${YELLOW}[04] 恢复IPv6${RESET}"
+    echo -e "${YELLOW}[05] 安装保活Oracle${RESET}"
+    echo -e "${YELLOW}[06] 安装lookbusy保活${RESET}"
+    echo -e "${YELLOW}[07] 安装Y探长${RESET}"
+    echo -e "${YELLOW}[08] 安装oci-start${RESET}"
     echo -e "${GREEN}[88] 更新脚本${RESET}"
     echo -e "${GREEN}[99] 卸载脚本${RESET}"
-    echo -e "${GREEN}[ 0] 退出${RESET}"
-    echo -e "${GREEN}-------------------------${RESET}"
-    read -p "请选择: " choice
+    echo -e "${GREEN}[00] 退出${RESET}"
+    echo -ne "${RED}请选择: ${RESET}"
+    read choice
     case $choice in
         1)  bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/oracle/main/ocibrick.sh) && pause ;;
         2)  bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/tool/main/xgroot.sh) && pause ;;
@@ -49,7 +50,7 @@ menu() {
         5)  bash <(wget -qO- --no-check-certificate https://gitlab.com/spiritysdx/Oracle-server-keep-alive-script/-/raw/main/oalive.sh) && pause ;;
         6)  bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/oracle/main/lookbusy.sh) && pause ;;
         7)  bash <(wget -qO- https://github.com/Yohann0617/oci-helper/releases/latest/download/sh_oci-helper_install.sh) && pause ;;
-        8)  bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/app-store/main/oci-start.sh) && pause ;;
+        8)  bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/oracle/main/oci-start.sh) && pause ;;
         88)
             echo -e "${YELLOW}🔄 正在更新脚本...${RESET}"
             curl -fsSL -o "$SCRIPT_PATH" "$SCRIPT_URL"
@@ -65,7 +66,7 @@ menu() {
             echo -e "${GREEN}✅ 卸载完成${RESET}"
             exit 0
             ;;
-        0) exit 0 ;;
+        00) exit 0 ;;
         *) echo -e "${RED}无效选择，请重新输入${RESET}" && pause ;;
     esac
     menu
