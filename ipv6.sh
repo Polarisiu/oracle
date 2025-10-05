ed20b69851fa43057ab0c2cec4589e75da27993f
Polaris <fdyrwaas@outlook.com>
2025-10-05 10:00:56 +0800

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 确认提示
-read -p $'\033[31m即将运行脚本 jhb.ovh/jb/v6.sh，是否继续？(y/n): \033[0m' choice
+read -p $'\033[31m即将运行添加V6是否继续？(y/n): \033[0m' choice
 
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     echo -e "\033[31m开始执行脚本...\033[0m"
